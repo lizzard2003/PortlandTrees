cf393e40ea18c44e4d65b02237a87929fea14abc
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2025-06-16 20:43:07 -0500

    second

--- PortlandTreesDataCleanup.py
+++ PortlandTreesDataCleanup.py
@@ -2,12 +2,25 @@
 
 import pandas as pd
 
-# This is the location of the file we are going to be looking at
-file_path = "/Users/liz/PortlandTreeDataClean/PortlandTrees.csv"
+try:
+    # This is the location of the file we are going to be looking at
+    file_path = "/Users/liz/PortlandTreeDataClean/PortlandTrees.csv"
 
-# we make a data frame from the CSV files
-df = pd.read_csv(file_path)
+    # we make a data frame from the CSV files
+    df = pd.read_csv(file_path)
 
-# We Display the first 5 rows of the Data frame
-print("First 5 rows of the DataFrame:")
-print(df.head())
+    # We Display the first 7 rows of the Data frame
+    # print("First 7 rows of the DataFrame:")
+    # print(df.head())
+    print(df.head(7))
+
+    print("The info of the dataframe")
+    df.info()  # this tells up how many colums we have and their name
+    print("This is describe:")
+    print(df.describe(include="all"))
+except FileNotFoundError:
+    print(
+        f"Error: The file '{file_path}' was not found. Please ensure the file is in the correct directory."
+    )
+except Exception as e:
+    print(f"An error occurred: {e}")
