041e38e46e842ac351f91ac5ccaeeb217fdde3fe
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2025-07-20 11:18:19 -0500

    html save

--- PortlandTreesDataCleanup.py
+++ PortlandTreesDataCleanup.py
@@ -64,9 +64,9 @@ def mapit(df):
             ).add_to(portland_map)
 
         # Save the map to an HTML file
-        portland_map.save("portland_tree_map.html")
-        webbrowser.open("portland_tree_map.html")
-        print("Map saved as 'portland_tree_map.html'")
+        portland_map.save("index.html")
+        webbrowser.open("index.html")
+        print("Map saved as 'index.html'")
     else:
         print("Latitude and Longitude columns not found in dataset.")
 
